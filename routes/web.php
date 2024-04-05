2cf81162d508c407d3602abece8c06b54b5495e9
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2024-04-05 11:21:34 +0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -68,3 +69,4 @@ Route::resource('siswa', SiswaController::class)->middleware(['auth', 'admin']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('upload', UploadController::class);
